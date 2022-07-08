1a6eba9e66746dad524e9ac5b3a255e59b660094
Prateek Rao <raopg@uci.edu>
2022-07-08 00:55:49 -0400

    fix tailwind config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from "next";
 import Emoji from "@/components/Emoji";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex ">
-      <Emoji label="waving-hand" symbol="👋" classes="animate-waving-hand" />
+    <div className="h-screen w-screen flex justify-center items-center">
+      <Head>
+        <title>Prateek Rao</title>
+      </Head>
+      <Emoji
+        label="waving-hand"
+        symbol="👋"
+        classes="animate-waving-hand text-6xl"
+      />
     </div>
   );
 };
