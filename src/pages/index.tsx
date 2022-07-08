aa0f7535556c8aeb93c30970313d414628c0aaac
Prateek Rao <raopg@uci.edu>
2022-07-08 01:30:08 -0400

    add buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import Emoji from "@/components/Emoji";
 import Head from "next/head";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
 
 const Home: NextPage = () => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
       <Head>
         <title>Prateek Rao</title>
       </Head>
@@ -13,6 +15,18 @@ const Home: NextPage = () => {
         symbol="👋"
         classes="animate-waving-hand text-6xl"
       />
+      <div className="text-4xl p-8 ml-8">I&#8217;m Prateek Rao</div>
+      <div className="flex flex-row justify-space-between">
+        <button className="w-10 h-10 flex justify-center items-center">
+          <BsGithub className="text-2xl" />
+        </button>
+        <button className="w-10 h-10 flex justify-center items-center">
+          <BsLinkedin className="text-2xl" />
+        </button>
+        <button className="w-10 h-10 flex justify-center items-center">
+          <MdEmail className="text-2xl" />
+        </button>
+      </div>
     </div>
   );
 };
