b9a11996ab2d378e353478f6bd87b73e1d731027
Prateek Rao <raopg@uci.edu>
2022-07-08 02:24:20 -0400

    add resume link

--- src/constants/externalLinks.ts
+++ src/constants/externalLinks.ts
@@ -1,3 +1,4 @@
 export const GITHUB_PROFILE = "https://github.com/raopg";
 export const LINKEDIN_PROFILE = "https://linkedin.com/in/raopg";
 export const EMAIL = "mailto:prateek.rao.08@gmail.com";
+export const RESUME = "assets/Resume.pdf";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,12 @@ import type { NextPage } from "next";
 import { Emoji, Link } from "@components";
 import Head from "next/head";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
+import { MdEmail, MdDownload } from "react-icons/md";
 import {
   EMAIL,
   GITHUB_PROFILE,
   LINKEDIN_PROFILE,
+  RESUME,
 } from "@constants/externalLinks";
 
 const Home: NextPage = () => {
@@ -16,8 +17,8 @@ const Home: NextPage = () => {
         <title>Prateek Rao</title>
       </Head>
       <Emoji label="waving-hand" symbol="👋" classes="animate-wave text-6xl" />
-      <div className="text-4xl p-8 ml-8">I&#8217;m Prateek Rao</div>
-      <div className="flex flex-row justify-space-between">
+      <div className="text-4xl p-8">I&#8217;m Prateek Rao</div>
+      <div className="flex flex-row justify-center">
         <Link to={GITHUB_PROFILE}>
           <BsGithub className="text-2xl cursor-pointer" />
         </Link>
@@ -26,6 +27,9 @@ const Home: NextPage = () => {
         </Link>
         <Link to={EMAIL}>
           <MdEmail className="text-3xl cursor-pointer" />
+        </Link>
+        <Link to={RESUME}>
+          <MdDownload className="text-3xl cursor-pointer" />
         </Link>
       </div>
     </div>
